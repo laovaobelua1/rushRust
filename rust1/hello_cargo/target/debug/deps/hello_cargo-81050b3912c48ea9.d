f8b6ff721207edf1a9c9c23949c8001349144e8b
ko qtam <huy@LVBN.local>
2026-01-01 22:10:03 +0700

    remove hello_cargo

--- rust1/hello_cargo/target/debug/deps/hello_cargo-81050b3912c48ea9.d
+++
@@ -1,5 +0,0 @@
-/Users/huy/Documents/CODE-HUY/rust/rust1/hello_cargo/target/debug/deps/hello_cargo-81050b3912c48ea9.d: src/main.rs
-
-/Users/huy/Documents/CODE-HUY/rust/rust1/hello_cargo/target/debug/deps/hello_cargo-81050b3912c48ea9: src/main.rs
-
-src/main.rs:
--- rust1/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/huy/Documents/CODE-HUY/rust/rust1/hello_cargo/target/debug/hello_cargo: /Users/huy/Documents/CODE-HUY/rust/rust1/hello_cargo/src/main.rs
